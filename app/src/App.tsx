d662bb5c29ba0ee3805e2b957da35fcb943bcf30
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-07-17 15:51:43 +0200

    add footer at the page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,14 @@
 import NavBar from "../assets/components/header";
+import Footer from "../assets/components/footer";
 
 export default function App() {
   return (
     <>
       <div className="App">
         <NavBar />
+      </div>
+      <div className="App">
+        <Footer />
       </div>
     </>
   );
